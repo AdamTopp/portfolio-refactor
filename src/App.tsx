934f7eff5121d4b21ec4990da679a3deb5d78361
AdamTopmay <adamtoppmayer@gmail.com>
2024-06-24 17:43:50 +0200

    [PR-1] Init files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TitleScreen from './TitleScreen/TitleScreen'
+
+function App() {
+  return (
+    <div>
+      <TitleScreen />
+    </div>
+  )
+}
+
+export default App
---
+++ src/TitleScreen/TitleScreen.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TitleScreen() {
+  return (
+    <div>Adam</div>
+  )
+}
+
+export default TitleScreen
